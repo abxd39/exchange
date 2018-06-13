e85d17aa39b94e27aada4cecbf96ab35bc9c7726
wyw <764137119@qq.com>
2018-06-13 13:47:40 +0800

    .\user_service\dao\mysql.go

--- user_service/rpc/handler/noticeHandler.go
+++ user_service/rpc/handler/noticeHandler.go
@@ -3,26 +3,25 @@ package handler
 import (
 	proto "digicon/proto/rpc"
 	. "digicon/user_service/dao"
-	"digicon/user_service/model"
 
 	"golang.org/x/net/context"
 )
 
-func (s *Greeter) NotiveList(ctx context.Context, req *proto.NoticeListRequest, rsp *proto.NoticeListResponse) error {
-	result := make([]model.NoticeStruct, 0)
-	ok := DB.NoticeList(&result)
+func (s *RPCServer) NotiveList(ctx context.Context, req *proto.NoticeListRequest, rsp *proto.NoticeListResponse) error {
+	//result := make([]model.NoticeStruct, 0)
+	result, ok := DB.NoticeList()
 	if ok {
 		len := len(result)
 		for _, value := range result {
 			ntc := proto.NoticeListResponse_Notice{}
-			ntc.Id = value.Id
+			ntc.Id = value.ID
 		}
 
 	}
 	return nil
 }
 
-func (s *Greeter) NotiveDetail(ctx context.Context, req *proto.NoticeDetailRequest, rsp *proto.NoticeDetailResponse) error {
+func (s *RPCServer) NotiveDetail(ctx context.Context, req *proto.NoticeDetailRequest, rsp *proto.NoticeDetailResponse) error {
 
 	return nil
 }
