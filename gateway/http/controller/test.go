f6adbf69f3a6d95130f2cae3a667d8632eac600f
wyw <764137119@qq.com>
2018-06-13 16:46:02 +0800

    all

--- gateway/http/controller/test.go
+++ gateway/http/controller/test.go
@@ -2,19 +2,22 @@ package controller
 
 import (
 	"digicon/gateway/rpc"
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func HelloController(c *gin.Context) {
 
-	s, _ := c.Params.Get("name")
+	//s, _ := c.Params.Get("name")
 
-	rsp, err := rpc.InnerService.UserSevice.CallGreet(s)
+	//rsp, err := rpc.InnerService.UserSevice.CallGreet(s)
+	rsp, err := rpc.InnerService.UserSevice.CallNoticeDesc(1)
 	if err != nil {
 		c.String(http.StatusOK, "err rsp")
 		return
 	}
 
-	c.String(http.StatusOK, rsp.Greeting)
+	//c.String(http.StatusOK, rsp)
+	c.JSON(http.StatusOK, rsp)
 }
