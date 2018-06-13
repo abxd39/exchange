15d6776e08f427bec51ed06fbd90e587286df6a4
wyw <764137119@qq.com>
2018-06-13 11:38:00 +0800

    添加文章相关结构体

--- user_service/model/user.go
+++ user_service/model/user.go
@@ -17,3 +17,31 @@ type UserEx struct {
 	RealName     string
 	IdentifyCard string
 }
+
+type NoticeStruct struct {
+	ID             int32
+	Description    string //重要 、一般
+	Title          string
+	CreateDateTime string
+}
+
+type NoticeDetailStruct struct {
+	ID            int32
+	Title         string
+	Description   string
+	Content       string
+	Covers        []byte
+	ContentImages []byte
+	Type          int32
+	TypeName      string
+	Author        string
+	Weight        int32
+	Shares        int32
+	Hits          int32
+	Comments      int32
+	DisplayMark   bool
+	CreateTime    string
+	UpdateTime    string
+	AdminID       int32
+	AdminNickname string
+}
