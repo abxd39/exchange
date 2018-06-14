6986467a5badfcd533aa7e705ca8354e95f55faa
wyw <764137119@qq.com>
2018-06-14 20:48:32 +0800

    debug

--- user_service/model/user.go
+++ user_service/model/user.go
@@ -10,31 +10,3 @@ type User struct {
 	GoogleVerifyId   string `xorm:"VARCHAR(128)"`
 	GoogleVerifyTime int    `xorm:"INT(255)"`
 }
-
-type ArticlesStruct struct {
-	ID             int32
-	Description    string //重要 、一般
-	Title          string
-	CreateDateTime string
-}
-
-type ArticlesDetailStruct struct {
-	ID            int32
-	Title         string
-	Description   string
-	Content       string
-	Covers        []byte
-	ContentImages []byte
-	Type          int32
-	TypeName      string
-	Author        string
-	Weight        int32
-	Shares        int32
-	Hits          int32
-	Comments      int32
-	DisplayMark   bool
-	CreateTime    string
-	UpdateTime    string
-	AdminID       int32
-	AdminNickname string
-}
