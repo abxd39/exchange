f30cc1c1507f8f31cbef983364814b6f93de6909
xutonghua <287835375@qq.com>
2018-06-19 10:00:42 +0800

    test

---
+++ wallet_service/model/blocks.go
@@ -0,0 +1,19 @@
+package models
+import(
+. "digicon/wallet_service/utils"
+)
+type Blocks struct {
+	Id     int    `xorm:"not null pk INT(11)"`
+	Number int    `xorm:"comment('区块高度') INT(11)"`
+	Txs    string `xorm:"comment('包含的交易') TEXT"`
+	Hash   string `xorm:"comment('区块hash') VARCHAR(48)"`
+}
+
+func (this *Blocks) Max_number ()(int,error){
+	_, err :=Engine.Select("max(number) number").Get(this)
+	if err != nil{
+		return 0,nil
+	}
+	//fmt.Println(ok,err,this)
+	return this.Number,nil
+}
--- wallet_service/model/tokens.go
+++ wallet_service/model/tokens.go
@@ -1,6 +1,6 @@
 package models
 
-import (
+import(
 	. "digicon/wallet_service/utils"
 )
 
@@ -8,12 +8,12 @@ type Tokens struct {
 	Id      int    `xorm:"not null pk INT(11)"`
 	Name    string `xorm:"comment('货币名称') VARCHAR(180)"`
 	Mark    string `xorm:"comment('英文标识') CHAR(30)"`
-	Chainid int64  `xorm:"comment('network id') BIGINT(20)"`
+	Chainid int    `xorm:"comment('network id') INT(20)"`
 	Eip155  int    `xorm:"default 1 comment('是否eip155加密') TINYINT(1)"`
 	Type    string `xorm:"comment('钱包生成方式(eth,btc)') CHAR(9)"`
 	Data    string `xorm:"comment('合约地址') CHAR(60)"`
 }
 
-func (this *Tokens) GetByid() (bool, error) {
+func (this *Tokens) GetByid()(bool, error){
 	return Engine.Id(this.Id).Get(this)
 }
