e5dcb4eb43a0372466afa86e49bc9c80fafd5a8c
xutonghua <287835375@qq.com>
2018-06-22 16:56:46 +0800

    erc20签名

--- gateway/http/controller/wallet.go
+++ gateway/http/controller/wallet.go
@@ -3,31 +3,56 @@ package controller
 import (
 	"digicon/gateway/rpc"
 	"github.com/gin-gonic/gin"
+	. "digicon/proto/common"
 	"net/http"
 	"strconv"
 )
 
 type WalletGroup struct {
+
 }
 
-func (this *WalletGroup) Router(router *gin.Engine) {
+func (this *WalletGroup) Router(router *gin.Engine){
 
 	r := router.Group("wallet")
-	r.GET("create", this.Create)
-	r.GET("update", this.Update)
-	r.GET("query", this.Query)
-	r.GET("delete", this.Delete)
-	r.GET("findOne", this.FindOne)
+	r.GET("create",this.Create)
+	r.GET("signtx",this.Signtx)
+	r.GET("tibi",this.Tibi)
+	r.GET("update",this.Update)
+	r.GET("query",this.Query)
+	r.GET("delete",this.Delete)
+	r.GET("findOne",this.FindOne)
 }
 
-func (this *WalletGroup) Index(ctx *gin.Context) {
+func (this *WalletGroup)Index(ctx *gin.Context){
 
 }
-func (this *WalletGroup) Create(ctx *gin.Context) {
-	userid, _ := strconv.Atoi(ctx.Query("userid"))
-	tokenid, _ := strconv.Atoi(ctx.Query("tokenid"))
+func (this *WalletGroup)Create(ctx *gin.Context){
+	userid ,_	:= strconv.Atoi(  ctx.Query("uid"))
+	tokenid ,_	:= strconv.Atoi(   ctx.Query("token_id"))
 
-	rsp, err := rpc.InnerService.WalletSevice.CallCreateWallet(userid, tokenid)
+	rsp, err := rpc.InnerService.WalletSevice.CallCreateWallet(userid,tokenid)
+	if err != nil {
+		ctx.String(http.StatusOK, err.Error())
+		return
+	}
+
+	ctx.JSON(http.StatusOK, rsp)
+}
+func (this *WalletGroup)Signtx(ctx *gin.Context){
+	userid ,err1	:= strconv.Atoi(  ctx.Query("uid"))
+	tokenid ,err2	:= strconv.Atoi(   ctx.Query("token_id"))
+	//to := "0x8e430b7fc9c41736911e1699dbcb6d4753cbe3b6"
+	to := ctx.Query("to")
+	gasprice,err3 := strconv.Atoi( ctx.Query("gasprice"))
+	amount := ctx.Query("amount")
+	if err1 != nil || err2!=nil || err3!=nil {
+		ctx.String(http.StatusOK, "参数错误")
+		return
+	}
+
+
+	rsp, err := rpc.InnerService.WalletSevice.CallSigntx(userid,tokenid,to,gasprice,amount)
 	if err != nil {
 		ctx.String(http.StatusOK, "err rsp")
 		return
@@ -35,7 +60,7 @@ func (this *WalletGroup) Create(ctx *gin.Context) {
 
 	ctx.JSON(http.StatusOK, rsp)
 }
-func (this *WalletGroup) Update(ctx *gin.Context) {
+func (this *WalletGroup)Update(ctx *gin.Context){
 	rsp, err := rpc.InnerService.WalletSevice.Callhello("eth")
 	if err != nil {
 		ctx.String(http.StatusOK, "err 0000 rsp")
@@ -43,15 +68,43 @@ func (this *WalletGroup) Update(ctx *gin.Context) {
 	}
 	ctx.JSON(http.StatusOK, rsp)
 }
-func (this *WalletGroup) Query(ctx *gin.Context) {
+
+
+func (this *WalletGroup)Tibi(ctx *gin.Context){
+	ret := NewPublciError()
+	//defer func() {
+	//	ctx.JSON(http.StatusOK, ret.GetResult())
+	//}()
+	type Param struct {
+		Uid       int32 `form:"uid" binding:"required"`
+		Token_id  int32 `form:"token_id" binding:"required"`
+		To		  string `form:"to" binding:"required"`
+		Amount    string `form:"amount" binding:"required"`
+		Gasprice  int32 `form:"gasprice" binding:"required"`
+	}
+	var param Param
+	if err := ctx.ShouldBind(&param); err != nil {
+		ret.SetErrCode(ERRCODE_PARAM, err.Error())
+		return
+	}
+	rsp, err := rpc.InnerService.WalletSevice.CallTibi(param.Uid,param.Token_id,param.To,param.Gasprice,param.Amount)
+	if err != nil {
+		ctx.String(http.StatusOK, err.Error())
+		return
+	}
+	ctx.JSON(http.StatusOK, rsp)
+	return
+}
+
+func (this *WalletGroup)Query(ctx *gin.Context){
 
 }
-func (this *WalletGroup) Delete(ctx *gin.Context) {
+func (this *WalletGroup)Delete(ctx *gin.Context){
 
 }
-func (this *WalletGroup) FindOne(ctx *gin.Context) {
+func (this *WalletGroup)FindOne(ctx *gin.Context){
 
 }
-func (this *WalletGroup) before() gin.HandlerFunc {
-	return nil
+func (this *WalletGroup)before() gin.HandlerFunc {
+		return nil
 }
--- gateway/rpc/client/wallet.go
+++ gateway/rpc/client/wallet.go
@@ -13,8 +13,7 @@ import (
 type WalletRPCCli struct {
 	conn proto.Gateway2WallerService
 }
-
-func NewWalletRPCCli() (w *WalletRPCCli) {
+func NewWalletRPCCli() (w *WalletRPCCli){
 	consul_addr := cf.Cfg.MustValue("consul", "addr")
 	r := consul.NewRegistry(registry.Addrs(consul_addr))
 	service := micro.NewService(
@@ -32,7 +31,7 @@ func NewWalletRPCCli() (w *WalletRPCCli) {
 
 }
 
-func (this *WalletRPCCli) Callhello(name string) (rsp *proto.HelloResponse2, err error) {
+func (this *WalletRPCCli)Callhello(name string) (rsp *proto.HelloResponse2,err error){
 	rsp, err = this.conn.Hello(context.TODO(), &proto.HelloRequest2{Name: name})
 	if err != nil {
 		Log.Errorln(err.Error())
@@ -41,8 +40,24 @@ func (this *WalletRPCCli) Callhello(name string) (rsp *proto.HelloResponse2, err
 	return
 }
 
-func (this *WalletRPCCli) CallCreateWallet(userid int, tokenid int) (rsp *proto.CreateWalletResponse, err error) {
-	rsp, err = this.conn.CreateWallet(context.TODO(), &proto.CreateWalletRequest{Userid: int32(userid), Tokenid: int32(tokenid)})
+func (this *WalletRPCCli)CallCreateWallet(userid int,tokenid int) (rsp *proto.CreateWalletResponse,err error){
+	rsp, err = this.conn.CreateWallet(context.TODO(), &proto.CreateWalletRequest{Userid: int32(userid),	Tokenid:int32(tokenid)})
+	if err != nil {
+		Log.Errorln(err.Error())
+		return
+	}
+	return
+}
+func (this *WalletRPCCli)CallSigntx(userid int,tokenid int,to string,gasprice int,mount string) (rsp *proto.SigntxResponse,err error){
+	rsp, err = this.conn.Signtx(context.TODO(), &proto.SigntxRequest{Userid: int32(userid),	Tokenid:int32(tokenid),To:to,Gasprice:int32(gasprice),Mount:mount})
+	if err != nil {
+		Log.Errorln(err.Error())
+		return
+	}
+	return
+}
+func (this *WalletRPCCli)CallTibi(uid int32,token_id int32,to string,gasprice int32,amount string) (rsp *proto.TibiResponse,err error){
+	rsp, err = this.conn.Tibi(context.TODO(), &proto.TibiRequest{Uid: (uid),	TokenId:(token_id),To:to,Gasprice:(gasprice),Amount:amount})
 	if err != nil {
 		Log.Errorln(err.Error())
 		return
