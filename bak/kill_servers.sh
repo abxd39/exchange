987c0a536bba3d3329aabe66831dc622423a4dec
lijun <jun1484528313@gmail.com>
2018-06-25 17:11:46 +0800

    kill server shell

--- bak/kill_servers.sh
+++ bak/kill_servers.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
 
 
-services="gateway  currency_service public_service token_service user_service wallet_service  kline_service  root"
+services="gateway  currency_service public_service token_service user_service wallet_service  kline_service"
 for service in $services;do
     service_id=`ps -ef |grep -v grep | grep $service | awk -F " "  '{print $2}'`
     echo "kill $service,$service_id"
