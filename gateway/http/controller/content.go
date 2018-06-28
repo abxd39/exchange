a3919b60518c4d3aad88c03a40f0465f73c4a9d2
WangYingWen <764137119@qq.com>
2018-06-28 13:42:43 +0800

    add friendly link

---
+++ gateway/http/controller/content.go
@@ -0,0 +1,93 @@
+package controller
+
+import (
+	"digicon/gateway/log"
+	"digicon/gateway/rpc"
+	Err "digicon/proto/common"
+	proto "digicon/proto/rpc"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type ContentManage struct{}
+
+func (c *ContentManage) Router(r *gin.Engine) {
+	cont := r.Group("/content")
+
+	{
+		cont.POST("/b", c.Banner)
+		cont.POST("/addlink", c.AddFriendlyLink)
+		cont.GET("/linklist", c.GetFriendlyLink)
+	}
+}
+
+func (cm *ContentManage) Banner(c *gin.Context) {
+	ret := Err.NewPublciError()
+	defer func() {
+		c.JSON(http.StatusOK, ret.GetResult())
+	}()
+	return
+}
+
+func (cm *ContentManage) AddFriendlyLink(c *gin.Context) {
+	ret := Err.NewPublciError()
+	defer func() {
+		c.JSON(http.StatusOK, ret.GetResult())
+	}()
+	req := struct {
+		WebName   string `form:"web_name" json:"web_name" binding:"required"`
+		LinkName  string `form:"link_name" json:"link_name" binding:"required"`
+		Aorder    int32  `form:"order" json:"order" binding:"required"`
+		LinkState int32  `form:"link_state" json:"link_state" binding:"required"`
+	}{}
+	err := c.ShouldBind(&req)
+	if err != nil {
+		log.Log.Errorf(err.Error())
+		ret.SetErrCode(Err.ERRCODE_PARAM, err.Error())
+		return
+	}
+	rsp, err := rpc.InnerService.PublicService.CallAddFriendlyLink(&proto.AddFriendlyLinkRequest{
+		WebName:   req.WebName,
+		LinkName:  req.LinkName,
+		Aorder:    req.Aorder,
+		LinkState: req.LinkState,
+	})
+	if err != nil {
+		log.Log.Errorf(err.Error())
+		ret.SetErrCode(Err.ERRCODE_UNKNOWN)
+		return
+	}
+	ret.SetErrCode(rsp.Code)
+	return
+}
+
+func (cm *ContentManage) GetFriendlyLink(c *gin.Context) {
+	ret := Err.NewPublciError()
+	defer func() {
+		c.JSON(http.StatusOK, ret.GetResult())
+	}()
+
+	req := struct {
+		Page  int32 `form:"page" json:"page" binding:"required"`
+		Count int32 `form:"count" json:"count" `
+	}{}
+	err := c.ShouldBind(&req)
+	if err != nil {
+		log.Log.Errorf(err.Error())
+		ret.SetErrCode(Err.ERRCODE_PARAM, err.Error())
+		return
+	}
+	fmt.Println("2121211111111111111111")
+	fmt.Println(req)
+	rsp, err := rpc.InnerService.PublicService.CallGetFriendlyLink(&proto.FriendlyLinkRequest{})
+	if err != nil {
+		log.Log.Errorf(err.Error())
+		ret.SetErrCode(Err.ERRCODE_UNKNOWN)
+		return
+	}
+	ret.SetErrCode(rsp.Code)
+	ret.SetDataSection("list", rsp.Friend)
+	return
+}
---
+++ public_service/rpc/handler/friendly_handler.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"context"
+	proto "digicon/proto/rpc"
+	"digicon/public_service/log"
+	"digicon/public_service/model"
+	"fmt"
+)
+
+func (s *RPCServer) AddFriendlyLink(ctx context.Context, req *proto.AddFriendlyLinkRequest, rsp *proto.AddFriendlyLinkResponse) error {
+	f := model.FriendlyLink{}
+	err := f.Add(req, rsp)
+	if err != nil {
+		log.Log.Errorf(err.Error())
+	}
+	return nil
+}
+
+func (s *RPCServer) GetFriendlyLink(ctx context.Context, req *proto.FriendlyLinkRequest, rsp *proto.FriendlyLinkResponse) error {
+	fmt.Println("vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv")
+	f := model.FriendlyLink{}
+	err := f.GetFriendlyLinkList(req, rsp)
+	if err != nil {
+		log.Log.Errorf(err.Error())
+	}
+	return nil
+}
