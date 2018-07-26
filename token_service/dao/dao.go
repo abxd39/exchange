9d9c782413a2daad12ddc0e2f78c34f92a58c74c
陈伟伟 <326087571@qq.com>
2018-07-26 16:23:50 +0800

    读取g_common.tokens表

--- token_service/dao/dao.go
+++ token_service/dao/dao.go
@@ -3,18 +3,20 @@ package dao
 var DB *Dao
 
 type Dao struct {
-	redis  *RedisCli
-	mysql  *Mysql
-	mysql2 *Mysql2
+	redis       *RedisCli
+	mysql       *Mysql
+	mysql2      *Mysql2
+	commonMysql *MysqlCommon
 }
 
 func NewDao() (dao *Dao) {
 	mysql := NewMysql()
 	rediscli := NewRedisCli()
 	dao = &Dao{
-		redis:  rediscli,
-		mysql:  mysql,
-		mysql2: NewMysql2(),
+		redis:       rediscli,
+		mysql:       mysql,
+		mysql2:      NewMysql2(),
+		commonMysql: NewMysqlCommon(),
 	}
 	return
 }
